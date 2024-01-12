895d9534ac18869df35e7685286fe42edb4d8441
alexeylv <101318447+alexeylv@users.noreply.github.com>
2024-01-12 14:42:25 +0000

    Добавить строчки: если нужно активировать char то нужно убрать двойной слэш на соответствующих строчках, деактивировав двойным слэшом строчки содержащие float

--- .ccls-cache/@home@runner@Prieobrazovaniie-tipov/main.cpp
+++ .ccls-cache/@home@runner@Prieobrazovaniie-tipov/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-  char x = 112;
-  std::cout << x + 4 << std::endl; //результат можно посмотреть в таблице ACSII
+  //char num=4, num1='4'; //Убрать двойной слэш для активизации строчки
+  float fl=5.6, fl1=5.7;
+  std::cout<<fl+fl1<<std::endl;
+  //std::cout<<static_cast<int>(num+num1)<<std::endl; //результат можно посмотреть в таблице ACSII //Убрать двойной слэш для активизации строчки
 }
